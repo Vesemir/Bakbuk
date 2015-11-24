efd13645068aa90103bd891d7e7672bdf01bccab
Efremov Denis <orandge1000@gmail.com>
2015-11-24 23:46:28 +0300

    mongo added succesfully

---
+++ railswithmongo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railswithmongo::Application.config.secret_key_base = '93b87daeaa6b4894a695f99afcbeade9c7f2e12cec221815c73a004754cc387b4e714b9d2ed2ea65277919815bde32c36dd555aa54ff5f7abc86e7cf4f1f6889'
